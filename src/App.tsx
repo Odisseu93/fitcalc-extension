87f8e257646d486f212092fe4ef62b6f0e77bca0
Ulisses <ulissessuporteti01@gmail.com>
2023-04-30 20:18:21 -0300

    feat: add react.js

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <h1>Hello Wolrds!</h1>
+        </div>
+    )
+};
+
+export default App;
